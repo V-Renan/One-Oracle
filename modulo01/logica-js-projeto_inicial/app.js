1e292c4ed67a0233d4645e0286735e94d32757fa
V-Renan <victorrenan2018@gmail.com>
2024-01-20 00:50:02 -0300

    One
    modulo01

--- modulo01/logica-js-projeto_inicial/app.js
+++ modulo01/logica-js-projeto_inicial/app.js
@@ -1,7 +1,19 @@
 alert('Boas vindas ao jogo do numero secreto!');
-let numeroSecreto = 5;
-let resposta = prompt('Escolha um numero entre 1 e 10');
+let numeroSecreto = 9;
+let resposta;
+let cont = 0;
 
-if (numeroSecreto == resposta) {
-    console.log('voce acertou!');
+while (resposta != numeroSecreto) {
+    resposta = prompt('Escolha um numero entre 1 e 10');
+    cont ++;
+
+    if (resposta == numeroSecreto) {
+        alert(`Isso ai! Voce descobriu o numero secreto ${numeroSecreto}.\nVoce acertou em ${cont} tentativa(s)!`);
+    } else {
+        if (resposta > numeroSecreto) {
+            alert(`O numero secreto é menor que ${resposta}`);
+        } else {
+            alert(`O numero secreto é maior que ${resposta}`);
+        }
+    }
 }
